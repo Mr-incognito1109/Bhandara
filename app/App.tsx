cc68f15b143bd5ea515d358253529dd211c01d2b
Pratik Shukla <79405008+Mr-incognito1109@users.noreply.github.com>
2024-05-10 17:24:17 +0530

    added tailwind

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='bg-black h-full w-full'>
+      <Text className='text-3xl'>Hello this is testing</Text>
+    </View>
+  )
+}
+
+export default index
